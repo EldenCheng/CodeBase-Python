fe38d4c451a265a91305e291436bda62d697bc98
elden.zheng <elden.zheng@wesoft.com>
2022-01-11 15:51:45 +0800

    添加一些单独获取年,月,日,时,分,秒的示例

---
+++ Python/Date_Time/Datetime_day_hour_minute.py
@@ -0,0 +1,19 @@
+import datetime
+
+if __name__ == '__main__':
+    print("当前完整日期: ", datetime.datetime.now().date())
+
+    print("当前日期的年: ", datetime.datetime.now().year)
+
+    print("当前日期的月: ", datetime.datetime.now().month)
+
+    print("当前日期的日: ", datetime.datetime.now().day)
+
+    print("当前完整时间: ", datetime.datetime.now().time())
+
+    print("当前日期的时: ", datetime.datetime.now().hour)
+
+    print("当前日期的分: ", datetime.datetime.now().minute)
+
+    print("当前日期的秒: ", datetime.datetime.now().second)
+
