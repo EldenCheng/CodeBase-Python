635eeaed92ad2ce4724d331521d273cba0b86efb
elden <mcgods@sina.com>
2024-03-12 23:17:14 +0800

    添加指定视频文件时, 可以使用通配符

--- ffmpeg/merge.py
+++ ffmpeg/merge.py
@@ -3,25 +3,27 @@ from pathlib import Path
 
 '''
 命令行格式: Merge_Video.exe "path:/video1.mp4" ""path:/video2.mp4""
-注意下面有两个转换器, h264_mp4xxx用于转换h264, hevc_mp4xxx用于转换h265
+或者
+命令行格式: Merge_Video.exe "path:/*.视频格式""
 '''
 
 
 def merge_video(vname):
-
     merge_str = '"concat:'
-    ffmpeg_install = False
+    ffmpeg_install = True
     ffmpeg_path = "ffmpeg" if ffmpeg_install else str(Path('../ffmpeg/bin/').absolute()) + '/ffmpeg.exe'
     temp_list = list()
+    temp_folder = "./temp"
     # print("The params:", vname)
 
     if len(vname) == 1:
 
-        path_string = vname[0].split(r'/') 
+        path_string = vname[0].split(r'/')
+        # print(path_string)
 
         if len(path_string) > 1:  # 如果在vname中能找到"/"的话, split函数会把路径分为两部分
             file_filter = path_string[-1]  # 文件名是split后最后一个元素
-            folder_name = vname.replace(path_string[-1], '')  # 把文件名去掉就是文件夹路径了
+            folder_name = path_string[0].replace(path_string[-1], '')  # 把文件名去掉就是文件夹路径了
             # print(folder_name)
             folder_path = Path(folder_name)
         else:
@@ -30,41 +32,41 @@ def merge_video(vname):
             folder_path = Path(folder_name)
 
         all_files = list(folder_path.rglob(file_filter))  # 使用文件名去查找文件, rglob支持通配符, 能把符合条件的所有文件生成一个列表
+        all_files.sort()
     else:
         all_files = vname
 
+    # print(all_files)
+
     for v in all_files:
-        # if '.mp4' in v:
-        #     v = v.split('.')[0]
         # 因为需要先转换成.ts格式才方便合并
-        print("v is: ", str(v))
+        # print("v is: ", str(v))
         if str(v).find('ts') != -1:
             temp_video_path = str(Path(v).absolute())
         else:
-            temp_video_name = str(v)[:-4]  # 找出视频文件名称, 去除扩展名
-            temp_video_path = str(Path(v).parent.absolute()) + "/temp/" + temp_video_name + ".ts"
-            # print("Temp video path", temp_video_path)
-            cmd_convert = '{0} -i {1} -vcodec copy -acodec copy -vbsf h264_mp4toannexb {2}'.format(ffmpeg_path,v, temp_video_path)
-            # cmd_convert = '{0} -i {1} -vcodec copy -acodec copy -vbsf hevc_mp4toannexb {2}'.format(ffmpeg_path,v, temp_video_path)
-            # cmd_convert = '{0} -i {1} -vcodec copy -acodec copy -vbsf hevc {2}'.format(ffmpeg_path,v, temp_video_path)
-            # print(cmd_convert)
-            os.system(cmd_convert)
+            # temp_video_name = str(v)[:-4]
+            # print(str(v))
+            # 找出视频文件名称, 去除扩展名
+            last_slash = str(v).rfind(r'/')
+            # print(last_slash)
+            temp_video_name = str(v).split(".")[0][last_slash:]
+            temp_video_path = str(Path(temp_folder).absolute()) + temp_video_name + ".ts"
+            # print("Temp video path: ", temp_video_path)
+            cmd_convert_to_ts = '{0} -i {1} -movflags use_metadata_tags -map_metadata 0 -vcodec libx265 -crf 28 {2}'.format(
+                ffmpeg_path, v, temp_video_path)  # 先将原视频转换成h265, 文件名是xx.ts
+            os.system(cmd_convert_to_ts)
 
         # 将视频文件加入到合并列表
         merge_str = merge_str + '{0}|'.format(temp_video_path)
-        temp_list.append(Path(temp_video_path))
-        
+        temp_list.append(temp_video_path)
+
     merge_str = merge_str[:-1] + '"'  # 合并列表最后加"符号
 
     # 合并
-    cmd_merge = '{0} -i {1} -acodec copy -vcodec copy -absf aac_adtstoasc merged_video.mp4'.format(ffmpeg_path, merge_str)
+    # print("merge_str is ", merge_str)
+    cmd_merge = '{0} -i {1} -codec copy merged_video.ts'.format(ffmpeg_path, merge_str)
     os.system(cmd_merge)
-    # video_path = Path(vname[-1]).absolute()
-    # dir_name = str(video_path.parent)
-    # test = os.listdir(dir_name)
 
-    # for item in temp_list:
-    #     # if item.endswith(".ts"):
-    #     #     os.remove(os.path.join(dir_name, item))
-    #     if item.is_file():
-    #         os.remove(str(item))
+    for item in temp_list:
+        if Path(item).is_file():
+            os.remove(str(item))
