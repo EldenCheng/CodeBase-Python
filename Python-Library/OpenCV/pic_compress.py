ecf2a98ba33b1e4b928d6092134099630896f752
elden <mcgods@sina.com>
2024-03-12 23:52:19 +0800

    添加压缩图片的范围选择

--- Python-Library/OpenCV/pic_compress.py
+++ Python-Library/OpenCV/pic_compress.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     source_path = r"H:/Snaps/Temp/*.jpg"
     target_path = "D:/PhotoAlbum/JPG/"
     size = 50
-    quility = 50
+    quality = 50
+    start_number = 0
+    end_number = -1
     file_filter = None
 
     if source_path.find("*.jpg") != -1:
@@ -16,13 +18,16 @@ if __name__ == '__main__':
     else:
         all_files = [Path(source_path)]
 
+    all_files.sort()
+    all_files = all_files[start_number:end_number]  # 因为压缩有时需要的时间比较长, 有可能先指定压缩一部分图片
+
     p = Pool(10)  # 打开进程池
     for e in all_files:
         # print(e.absolute())
         # print(e.name)
         source_file = str(e.absolute())
         desc_file = target_path + str(e.name)
-        p.apply_async(CompressImage.compress, (source_file, desc_file, 'm', size / 100, quility), )  # 多进程入
+        p.apply_async(CompressImage.compress, (source_file, desc_file, 'm', size / 100, quality), )  # 多进程入
     
     p.close()
     p.join()
