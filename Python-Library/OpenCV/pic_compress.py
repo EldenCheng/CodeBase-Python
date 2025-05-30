c75531f1fafd08f9a5a13ce6f99bf631f0a1494e
elden <mcgods@sina.com>
2025-05-31 01:28:44 +0800

    使opencv支持中文路径

--- Python-Library/OpenCV/pic_compress.py
+++ Python-Library/OpenCV/pic_compress.py
@@ -1,35 +1,61 @@
 from multiprocessing import Pool
+import os
 from pathlib import Path
 from pic_utils import CompressImage
 
 if __name__ == '__main__':
-    source_path = r"H:/Snaps/Temp/*.jpg"
+    # source_path = r"./Temp/*.jpg"
+    source_path = r"G:\Photo(JPG)\相册的相\2019\dali\*.jpg"
+    # source_path = r"H:\Download\Met-Art\Models\Nikky A\[Met-Art] - 2009-12-10 - Nikky A - Polerina (x125)/*.jpg"
+    # target_path = "F:/JPG/"
     target_path = "D:/PhotoAlbum/JPG/"
+    compress_temp_path = 'H:/Snaps/compress_temp/'
+    thread_number = 10
     size = 50
     quality = 50
     start_number = 0
-    end_number = -1
+    end_number = 10
     file_filter = None
+    chinese_folder = True
+    shutdown = True
 
     if source_path.find("*.jpg") != -1:
         folder_path = Path(source_path.split("*.jpg")[0])
         file_filter = "*.jpg"
+        all_files = list(folder_path.rglob(file_filter))  # 使用文件名去查找文件, rglob支持通配符, 能把符合条件的所有文件生成一个列表
+    elif source_path.find("*.jpeg") != -1:
+        folder_path = Path(source_path.split("*.jpeg")[0])
+        file_filter = "*.jpeg"
+        all_files = list(folder_path.rglob(file_filter))  # 使用文件名去查找文件, rglob支持通配符, 能把符合条件的所有文件生成一个列表
+    elif source_path.find("*.png") != -1:
+        folder_path = Path(source_path.split("*.png")[0])
+        file_filter = "*.png"
         all_files = list(folder_path.rglob(file_filter))  # 使用文件名去查找文件, rglob支持通配符, 能把符合条件的所有文件生成一个列表
     else:
         all_files = [Path(source_path)]
 
     all_files.sort()
     all_files = all_files[start_number:end_number]  # 因为压缩有时需要的时间比较长, 有可能先指定压缩一部分图片
-
-    p = Pool(10)  # 打开进程池
+    
+    p = Pool(thread_number)  # 打开进程池
     for e in all_files:
         # print(e.absolute())
         # print(e.name)
         source_file = str(e.absolute())
-        desc_file = target_path + str(e.name)
-        p.apply_async(CompressImage.compress, (source_file, desc_file, 'm', size / 100, quality), )  # 多进程入
+        # compress_temp_folder = Path(compress_temp_path)
+        if chinese_folder:
+            compress_temp_file = str(Path(compress_temp_path + e.name).absolute())
+            os.system(f'copy \"{source_file}\" {compress_temp_file}')
+            source_file = compress_temp_file
+        desc_file = target_path + str(e.name).split(".")[0] + ".jpg"
+        desc_file = desc_file.replace(" ", "")
+        p.apply_async(CompressImage.compress, (source_file, desc_file, chinese_folder, 'g', size / 100, quality), )  # 多进程入
     
     p.close()
     p.join()
+    del_cmd = r'del /q compress_temp\*.*'
+    os.system(del_cmd)
+    if shutdown:
+        os.system("shutdown /s")
 
 
--- Python-Library/OpenCV/pic_utils.py
+++ Python-Library/OpenCV/pic_utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import time
 
 import pyguetzli
@@ -8,7 +9,7 @@ import mozjpeg_lossless_optimization
 class CompressImage:
 
     @staticmethod
-    def compress(pic_path: str, target_path: str, method: str = 'm', size: float = 0.5, quality: int = 50):
+    def compress(pic_path: str, target_path: str, chinese=False, method: str = 'm', size: float = 0.5, quality: int = 50):
         """
         使用Google的guetzli库来压缩图片, 注意这个库只支持jpeg并且比较慢
         :param pic_path: 相片的Path对象实例
@@ -18,10 +19,20 @@ class CompressImage:
         try:
             print("Reducing pic {0} size...".format(pic_path))
             begin_time = time.time()
-            pic = cv2.imread(pic_path)
+            # print("path: ", pic_path)
+            
+            if chinese:
+                pic = cv2.imdecode(np.fromfile(pic_path, dtype=np.uint8), -1)
+            else:
+                pic = cv2.imread(pic_path)
             height, width = pic.shape[:2]  # 获取原始分辨率
             resize_pic = cv2.resize(pic, (int(width * size), int(height * size)))  # 缩小图片
-            cv2.imwrite(target_path, resize_pic, [int(cv2.IMWRITE_JPEG_QUALITY), quality])  # 按照质量设置保存图片
+            
+            if chinese:
+                cv2.imencode(".jpg", resize_pic)[1].tofile(target_path)
+            else:
+                cv2.imwrite(target_path, resize_pic, [int(cv2.IMWRITE_JPEG_QUALITY), quality])  # 按照质量设置保存图片
+            # print("Reduce pic by opencv, used: ".format(pic_path), int(time.time() - begin_time))
             if method:  # 设置是否需要额外压缩
                 rb = open(str(target_path), 'rb').read()  # 压缩图片需要二进制数据
                 if method == "g":  # 使用google pyguetzli压缩, 效果大概可以压缩到原图1/4, 已经缩小并降低质量的图都要1分钟左右
