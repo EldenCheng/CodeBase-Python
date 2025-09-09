07c81ea4e64cb59ccbf344ee54744cea4d8af528
elden.zheng <elden.zheng@wesoft.com>
2025-09-09 11:05:51 +0800

    研究将appium2的程序文件打包,不用在每台机上安装

---
+++ Python-Library/Appium/Package_Appium2/how_to_package_appium2.py
@@ -0,0 +1,12 @@
+r"""
+appium2将
+
+set APPIUM_HOME=%cd%\external\.appium\
+set ANDROID_HOME=%cd%\external\android-sdk
+set PATH=%PATH%;%ANDROID_HOME%\platform-tools\;%cd%\external\nodejs\;
+node ".\external\appium\index.js"
+"""
+
+
+if __name__ == '__main__':
+    pass
