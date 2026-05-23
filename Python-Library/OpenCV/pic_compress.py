8348fbcba3dda8ae23f418a16e610d83e0a742ab
elden <mcgods@sina.com>
2026-05-23 18:36:34 +0800

    强制最终转换的图片格式为jpg

--- Python-Library/OpenCV/pic_compress.py
+++ Python-Library/OpenCV/pic_compress.py
@@ -6,12 +6,11 @@ from pic_utils import CompressImage
 
 if __name__ == '__main__':
 
-    profile = "srwy"
     profile_setting = {
         "srwy": {
             "source_sufix": ".png",
             "source_folder": r"F:\EMU\NS\citron(yuzu fork)\user\screenshots",
-            "target_path": "D:/PhotoAlbum/JPG/",
+            "target_path": "H:/Snaps/SRWY/Temp/",
             "size": 80,
             "quality": 30,
             "chinese_folder": True
@@ -19,40 +18,34 @@ if __name__ == '__main__':
         "skyrim":{
             "source_sufix": ".png",
             "source_folder": r"C:\Users\Elden\Videos\NVIDIA",
-            "target_path": "D:/PhotoAlbum/JPG/",
+            "target_path": "H:/Snaps/Skyrim/Temp/",
             "size": 50,
             "quality": 30,
             "chinese_folder": True
         },
     }
 
+    profile = "skyrim"
     thread_number = 48
-    method = None
+    method = "m"
     start_number = 0
     end_number = -1
     all_files = None
     shutdown = False
 
-    source_folder = profile_setting[profile]["source_folder"]
-    source_sufix = profile_setting[profile]["source_sufix"]
-    target_path = profile_setting[profile]["target_path"]
-    size = profile_setting[profile]["size"]
-    quality = profile_setting[profile]["quality"]
-    chinese_folder = profile_setting[profile]["chinese_folder"]
+    direct_path = None
+    source_path = profile_setting[profile]["source_folder"] + "\\*" + profile_setting[profile]["source_sufix"] if not direct_path else direct_path
+    target_path = profile_setting[profile]["target_path"] if profile_setting.get(profile) else "D:/PhotoAlbum/JPG/"
+    size = profile_setting[profile]["size"] if profile_setting.get(profile) else 50
+    quality = profile_setting[profile]["quality"] if profile_setting.get(profile) else 50
+    chinese_folder = profile_setting[profile]["chinese_folder"] if profile_setting.get(profile) else True
 
-
-    source_path = source_folder + "\\*" + source_sufix
     compress_temp_path = 'H:/Snaps/compress_temp/'
-    file_suffix = ("*.jpg", "*.JPG", "*.jpeg", "*.png", "*.PNG")
 
-    for fs in file_suffix:
-        if source_path.find(fs) != -1:
-            folder_path = Path(source_path.split(fs)[0])
-            all_files = list(folder_path.rglob(fs))  # 使用文件名去查找文件, rglob支持通配符, 能把符合条件的所有文件生成一个列表
-            break
+    if source_path.find(profile_setting[profile]["source_sufix"]) != -1:
+        all_files = list(Path(profile_setting[profile]["source_folder"]).rglob("*" + profile_setting[profile]["source_sufix"]))  # 使用文件名去查找文件, rglob支持通配符, 能把符合条件的所有文件生成一个列表
     else:
-        if not all_files:
-            all_files = [Path(source_path)]
+        all_files = [Path(source_path)]
 
     all_files.sort()
     all_files = all_files[start_number:end_number]  # 因为压缩有时需要的时间比较长, 有可能先指定压缩一部分图片
@@ -63,10 +56,14 @@ if __name__ == '__main__':
 
     for e in all_files:
         source_file = str(e.absolute())
-        desc_file = target_path + str(e.name).split(source_sufix)[0] + ".jpg"
+        desc_file = target_path + str(e.name)
         desc_file = desc_file.replace(" ", "")
+        if desc_file[-4:-3] == ".":  # 换成jpg的话体积会小很多
+            desc_file = desc_file[:-3] + "jpg"
+        else:
+            desc_file = desc_file[:-4] + "jpg"
         thread_pool.submit(CompressImage.compress, source_file, desc_file, chinese_folder, method, size / 100, quality)
-
+        # CompressImage.compress(source_file, desc_file, chinese_folder, method, size / 100, quality)
     thread_pool.shutdown(wait=True)
 
     del_cmd = r'del /q compress_temp\*.*'
